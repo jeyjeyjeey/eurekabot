82b5fcc197c8b44732a650f23dbdb060d32532be
jeyjeyjeey <jeyjeyjeey@gmail.com>
2018-03-26 17:02:34 +0900

    add finally sessionclose to dbaccessor

--- EurekaDbAccessor.py
+++ EurekaDbAccessor.py
@@ -53,27 +53,28 @@ class EurekaDbAccessor(DBAccessor):
         final_score = 0
         gb_tweet = None
         rec_exists = True
-        gb_tweet = (session
-                    .query(func.max(GBTweet.final_score))
-                    .filter(
-                        GBTweet.user_id == tw.user_id,
-                        GBTweet.meta_ids_name == tw.meta_ids_name,
-                        GBTweet.stage_mode == tw.stage_mode,
-                    )
-                    .group_by(
-                        GBTweet.user_id,
-                        GBTweet.meta_ids_name,
-                        GBTweet.stage_mode
-                    )
-                    .one())
+        try:
+            gb_tweet = (session
+                        .query(func.max(GBTweet.final_score))
+                        .filter(
+                            GBTweet.user_id == tw.user_id,
+                            GBTweet.meta_ids_name == tw.meta_ids_name,
+                            GBTweet.stage_mode == tw.stage_mode,
+                        )
+                        .group_by(
+                            GBTweet.user_id,
+                            GBTweet.meta_ids_name,
+                            GBTweet.stage_mode
+                        )
+                        .one())
 
-        if len(gb_tweet) != 0:
-            final_score = gb_tweet[0]
-        else:
-            rec_exists = False
-
-        session.commit()
-        session.close()
+            if len(gb_tweet) != 0:
+                final_score = gb_tweet[0]
+            else:
+                rec_exists = False
+        finally:
+            session.commit()
+            session.close()
 
         return final_score, rec_exists
 
@@ -84,17 +85,18 @@ class EurekaDbAccessor(DBAccessor):
         gb_tweet = None
         rec_exists = True
         try:
-            gb_tweet = (session
-                        .query(GBTweet.final_score)
-                        .filter(
-                            GBTweet.id == tw.tweet_id
-                        )
-                        .one())
-        except NoResultFound as e:
-            rec_exists = False
-
-        session.commit()
-        session.close()
+            try:
+                gb_tweet = (session
+                            .query(GBTweet.final_score)
+                            .filter(
+                                GBTweet.id == tw.tweet_id
+                            )
+                            .one())
+            except NoResultFound as e:
+                rec_exists = False
+        finally:
+            session.commit()
+            session.close()
 
         return gb_tweet.final_score if rec_exists else 0, rec_exists
 
@@ -103,12 +105,13 @@ class EurekaDbAccessor(DBAccessor):
         session = session_maker()
 
         rec_exists = True
-        gb_tweets = session.execute(
-            self.sql_best_score_by_user,
-            {'user_id': tw.user_id}).fetchall()
-
-        session.commit()
-        session.close()
+        try:
+            gb_tweets = session.execute(
+                self.sql_best_score_by_user,
+                {'user_id': tw.user_id}).fetchall()
+        finally:
+            session.commit()
+            session.close()
 
         gb_tweets_dict_list = []
         if gb_tweets is not None and len(gb_tweets) != 0:
@@ -131,23 +134,24 @@ class EurekaDbAccessor(DBAccessor):
         gb_tweet = None
         rec_exists = True
         try:
-            gb_tweet = (session
-                        .query(GBTweet)
-                        .with_for_update()
-                        .filter(
-                            GBTweet.id == target_tw.tweet_id
-                        )
-                        .one())
-        except NoResultFound as e:
-            rec_exists = False
+            try:
+                gb_tweet = (session
+                            .query(GBTweet)
+                            .with_for_update()
+                            .filter(
+                                GBTweet.id == target_tw.tweet_id
+                            )
+                            .one())
+            except NoResultFound as e:
+                rec_exists = False
 
-        if rec_exists:
-            gb_tweet.updated_at = datetime.now()
-            gb_tweet.final_score = tw.final_score
-            gb_tweet.is_score_edited = '1'
-
-        session.commit()
-        session.close()
+            if rec_exists:
+                gb_tweet.updated_at = datetime.now()
+                gb_tweet.final_score = tw.final_score
+                gb_tweet.is_score_edited = '1'
+        finally:
+            session.commit()
+            session.close()
 
         return rec_exists
 
@@ -158,22 +162,23 @@ class EurekaDbAccessor(DBAccessor):
         gb_tweet = None
         rec_exists = True
         try:
-            gb_tweet = (session
-                        .query(GBTweet)
-                        .with_for_update()
-                        .filter(
-                            GBTweet.id == target_tw.tweet_id
-                        )
-                        .one())
-        except NoResultFound as e:
-            rec_exists = False
+            try:
+                gb_tweet = (session
+                            .query(GBTweet)
+                            .with_for_update()
+                            .filter(
+                                GBTweet.id == target_tw.tweet_id
+                            )
+                            .one())
+            except NoResultFound as e:
+                rec_exists = False
 
-        if rec_exists:
-            session.delete(gb_tweet)
-            tw.record_mode = C.DB_RECORD_DELETE
-
-        session.commit()
-        session.close()
+            if rec_exists:
+                session.delete(gb_tweet)
+                tw.record_mode = C.DB_RECORD_DELETE
+        finally:
+            session.commit()
+            session.close()
 
         return rec_exists
 
@@ -200,11 +205,13 @@ class EurekaDbAccessor(DBAccessor):
             tw.is_score_edited,
             tw.is_valid_data
         )
-        session.add(gb_tweet)
-        tw.record_mode = C.DB_RECORD_INSERT
 
-        session.commit()
-        session.close()
+        try:
+            session.add(gb_tweet)
+            tw.record_mode = C.DB_RECORD_INSERT
+        finally:
+            session.commit()
+            session.close()
 
         return tw
 
@@ -215,61 +222,62 @@ class EurekaDbAccessor(DBAccessor):
         now = datetime.now()
 
         gb_tweet = None
-        if tw.rec_exists is not None and tw.rec_exists is True:
-            try:
-                gb_tweet = (session
-                            .query(GBTweet)
-                            .with_for_update()
-                            .filter(
-                                GBTweet.user_id == tw.user_id,
-                                GBTweet.meta_ids_name == tw.meta_ids_name,
-                                GBTweet.stage_mode == tw.stage_mode,
-                            )
-                            .one())
-            except NoResultFound as e:
-                # irregular
-                tw.rec_exists = False
+        try:
+            if tw.rec_exists is not None and tw.rec_exists is True:
+                try:
+                    gb_tweet = (session
+                                .query(GBTweet)
+                                .with_for_update()
+                                .filter(
+                                    GBTweet.user_id == tw.user_id,
+                                    GBTweet.meta_ids_name == tw.meta_ids_name,
+                                    GBTweet.stage_mode == tw.stage_mode,
+                                )
+                                .one())
+                except NoResultFound as e:
+                    # irregular
+                    tw.rec_exists = False
 
-        update_flg = None
-        if not tw.rec_exists:
-            gb_tweet = GBTweet(
-                now,
-                '0000-00-00 00:00:00',
-                tw.user_id,
-                tw.meta_ids_name,
-                tw.stage_mode,
-                tw.screen_name,
-                tw.tweet_id,
-                tw.post_date,
-                self.sanitize_itr_obj(tw.hash_tags),
-                tw.final_score,
-                tw.total_score,
-                self.sanitize_itr_obj(np.array(tw.total_score_probas).round(2)),
-                tw.post_datetime,
-                tw.text,
-                tw.is_score_edited,
-                tw.is_valid_data
-            )
-            session.add(gb_tweet)
-            tw.record_mode = C.DB_RECORD_INSERT
-        elif tw.final_score >= tw.prev_final_score:
-            gb_tweet.updated_at = now
-            gb_tweet.tweet_id = tw.tweet_id
-            gb_tweet.post_date = tw.post_date
-            gb_tweet.hash_tags = self.sanitize_itr_obj(tw.hash_tags)
-            gb_tweet.final_score = tw.final_score
-            gb_tweet.total_score = tw.total_score
-            gb_tweet.total_score_probas = self.sanitize_itr_obj(np.array(tw.total_score_probas).round(2))
-            gb_tweet.post_datetime = tw.post_datetime
-            gb_tweet.text = tw.text
-            gb_tweet.is_score_edited = tw.is_score_edited
-            gb_tweet.is_valid_data = tw.is_valid_data
-            tw.record_mode = C.DB_RECORD_UPDATE
-        elif tw.final_score < tw.prev_final_score:
-            tw.record_mode = C.DB_RECORD_NOT_UPDATE
-
-        session.commit()
-        session.close()
+            update_flg = None
+            if not tw.rec_exists:
+                gb_tweet = GBTweet(
+                    now,
+                    '0000-00-00 00:00:00',
+                    tw.user_id,
+                    tw.meta_ids_name,
+                    tw.stage_mode,
+                    tw.screen_name,
+                    tw.tweet_id,
+                    tw.post_date,
+                    self.sanitize_itr_obj(tw.hash_tags),
+                    tw.final_score,
+                    tw.total_score,
+                    self.sanitize_itr_obj(np.array(tw.total_score_probas).round(2)),
+                    tw.post_datetime,
+                    tw.text,
+                    tw.is_score_edited,
+                    tw.is_valid_data
+                )
+                session.add(gb_tweet)
+                tw.record_mode = C.DB_RECORD_INSERT
+            elif tw.final_score >= tw.prev_final_score:
+                gb_tweet.updated_at = now
+                gb_tweet.tweet_id = tw.tweet_id
+                gb_tweet.post_date = tw.post_date
+                gb_tweet.hash_tags = self.sanitize_itr_obj(tw.hash_tags)
+                gb_tweet.final_score = tw.final_score
+                gb_tweet.total_score = tw.total_score
+                gb_tweet.total_score_probas = self.sanitize_itr_obj(np.array(tw.total_score_probas).round(2))
+                gb_tweet.post_datetime = tw.post_datetime
+                gb_tweet.text = tw.text
+                gb_tweet.is_score_edited = tw.is_score_edited
+                gb_tweet.is_valid_data = tw.is_valid_data
+                tw.record_mode = C.DB_RECORD_UPDATE
+            elif tw.final_score < tw.prev_final_score:
+                tw.record_mode = C.DB_RECORD_NOT_UPDATE
+        finally:
+            session.commit()
+            session.close()
 
         return tw
 
